9dcdc5cf5bff7a9d0fedc6a94e5d3aeeb4b6ac17
LEOCarrow <leeziao@hotmail.com>
2015-08-12 15:36:59 +0800

    jkfsld

--- funcatching/initializemap.cpp
+++ funcatching/initializemap.cpp
@@ -5,6 +5,12 @@ initializemap::initializemap(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::initializemap)
 {
+    QRegExp regExp("[1-9][0-9]{0,7}");
+//    ui->columnEdit->setValidator(new QRegExpValidator(regExp));
+//    ui->rowEdit->setValidator(new QRegExpValidator(regExp));
+
+    directory_exist = false;
+    position_checked = false;
     ui->setupUi(this);
 }
 
@@ -15,7 +21,13 @@ initializemap::~initializemap()
 
 void initializemap::on_posCheck_clicked()
 {
-
+    if(!directory_exist){
+        ui->posCheck->setChecked(false);
+        return;
+    }
+    position_checked = !position_checked;
+    ui->rowEdit->setEnabled(position_checked);
+    ui->columnEdit->setEnabled(position_checked);
 }
 
 void initializemap::on_cancelButton_clicked()
@@ -35,5 +47,28 @@ void initializemap::on_listWidget_currentRowChanged(int currentRow)
 
 void initializemap::on_directoreButton_clicked()
 {
+    QString filename = QFileDialog::getOpenFileName(this,tr("choose the edit map"),".",tr("map(*.map)"));
+    QFile file(filename);
 
+    if(!file.open(QIODevice::ReadOnly)){
+        QMessageBox::warning(this,tr("Map editor"),
+                             tr("failed to read file %1:\n%2")
+                             .arg(file.fileName())
+                             .arg(file.errorString()));
+        return;
+    }
+    QDataStream in(&file);
+    in.setVersion(QDataStream::Qt_4_8);
+
+    quint32 magic;
+    in>>magic;
+
+    if(magic!=MagicNum){
+        QMessageBox::warning(this,tr("Map editor"),
+                             tr("This file is mot a Map file\nPlease rechoose the edited file"));
+        return;
+    }
+    ui->directoryLabel->setText(filename);
+    ui->listWidget->addItem("new");
+    directory_exist = true;
 }
--- funcatching/initializemap.h
+++ funcatching/initializemap.h
@@ -2,6 +2,8 @@
 #define INITIALIZEMAP_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QtGui>
 
 namespace Ui {
 class initializemap;
@@ -17,16 +19,15 @@ public:
 
 private slots:
     void on_posCheck_clicked();
-
     void on_cancelButton_clicked();
-
     void on_okButton_clicked();
-
     void on_listWidget_currentRowChanged(int currentRow);
-
     void on_directoreButton_clicked();
 
 private:
+    enum{MagicNum = 0x2FA3120C};
+    bool directory_exist;
+    bool position_checked;
     Ui::initializemap *ui;
 };
 
