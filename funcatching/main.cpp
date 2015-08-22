51565788440554551ddc034026a87aae17e4a0d8
LEOCarrow <leeziao@hotmail.com>
2015-08-22 15:56:38 +0800

    alks

--- funcatching/main.cpp
+++ funcatching/main.cpp
@@ -1,13 +1,12 @@
-#include "mainwindow.h"
 #include <QApplication>
-#include <QDebug>
 #include <QTextCodec>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
-    MainWindow w;
-    w.show();
+    MainWindow *w = new MainWindow;
+    w->show();
     return app.exec();
 }
